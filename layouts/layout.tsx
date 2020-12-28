93cd7ee626d7535557bdc23fcfba322935189f18
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2020-12-29 00:32:35 +0100

    fix dark mode script

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -11,10 +11,11 @@ export default function Layout({children}) {
     
     return(
         <>
-            <script dangerouslySetInnerHTML={{ __html: nightwind.init() }} />
             <Head>
                 <title>{title}</title>
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
+                
+                <script dangerouslySetInnerHTML={{ __html: nightwind.init() }} />
                 
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="description" content={description} />
